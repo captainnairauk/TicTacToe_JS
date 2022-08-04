73f245dccef337cd250b3e09598cb6ef106da69e
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-08-05 00:15:10 +0530

    Added handleResultValidation function.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const playerDisplay = document.querySelector(".display-player");
 const resetButton = document.querySelector("#reset");
 const announcer = document.querySelector(".announcer");
 
-let board = ["", "", "", "", "", "","", "", ""];
+let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let isGameActive = true;
 
@@ -12,18 +12,45 @@ const PLAYER0_WON = "PLAYER0_ WON";
 const TIE = "TIE";
 
 const winningConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a == "" || b == "" || c == "") {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        announce(currentPlayer === "X" ? PLAYERX_WON : PLAYER0_WON);
+        isGameActive = false;
+        return;
+    }
+
+    if (!board.includes("")){
+        announce(TIE);
+    }
+};
+
 const announce = (type) => {
-    switch(type){
+    switch (type) {
         case PLAYER0_WON:
             announcer.innerHTML = 'Player <span class = "player0">0</span> Won';
             break;
@@ -41,18 +68,18 @@ const changePlayer = () => {
     currentPlayer = currentPlayer === "X" ? "0" : "X";
     playerDisplay.innerText = currentPlayer;
     playerDisplay.classList.add(`player${currentPlayer}`);
-}
+};
 
-const userAction = (tile, index) =>{
-    if(isValidAction(tile) && isGameActive) {
+const userAction = (tile, index) => {
+    if (isValidAction(tile) && isGameActive) {
         tile.innerText = currentPlayer;
         tile.classList.add(`player${currentPlayer}`);
         updateBoard(index);
         handleResultValidation();
         changePlayer();
-    }
-}
+    };
+};
 
-tiles.forEach( (tile,index) => {
+tiles.forEach((tile, index) => {
     tile.addEventListener("click", () => userAction(tile, index));
 });
