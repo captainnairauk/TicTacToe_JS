7d53b2abe1a565aa8ea1166684819afba37b301c
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-08-04 23:30:34 +0530

    Created variables for game outcomes.

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const announcer = document.querySelector(".announcer");
 let board = ["", "", "", "", "", "","", "", ""];
 let currentPlayer = "X";
 let isGameActive = true;
+
+const PLAYERX_WON = "PLAYERX_WON";
+const PLAYER0_WON = "PLAYER0_ WON";
+const TIE = "TIE";
