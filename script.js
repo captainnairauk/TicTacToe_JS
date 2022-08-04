921fb551e27cc3a5b840de7faad70449f83cb8fc
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-08-04 23:34:44 +0530

    Created variables to define game flow.

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ let isGameActive = true;
 const PLAYERX_WON = "PLAYERX_WON";
 const PLAYER0_WON = "PLAYER0_ WON";
 const TIE = "TIE";
+
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
